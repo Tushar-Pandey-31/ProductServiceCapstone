9ecfcb7683193cdce872b48e5b5e7e96a4e752af
tuxsharx <pandeytushar359@gmail.com>
2025-03-20 16:46:01 +0530

    adding response entity

--- src/main/java/com/scaler/productservicejan31capstone/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/scaler/productservicejan31capstone/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.scaler.productservicejan31capstone.exceptions;
 import com.scaler.productservicejan31capstone.dtos.ErrorDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,13 +19,15 @@ public class GlobalExceptionHandler
 
     }
     @ExceptionHandler(ProductNotFoundException.class)
-    public ErrorDto handleProductNotFoundException(
+    public ResponseEntity<ErrorDto> handleProductNotFoundException(
             ProductNotFoundException productNotFoundException
     )
     {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setStatus("Failure");
         errorDto.setMessage(productNotFoundException.getMessage());
-        return errorDto;
+
+        ResponseEntity<ErrorDto> responseEntity = new ResponseEntity<>(errorDto, HttpStatus.NOT_FOUND);
+        return responseEntity;
     }
 }
