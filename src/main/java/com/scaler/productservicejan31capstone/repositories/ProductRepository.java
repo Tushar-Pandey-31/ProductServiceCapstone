87922c686168e370674d3c1766aeb0e6614e775a
tuxsharx <pandeytushar359@gmail.com>
2025-04-06 21:59:08 +0530

    db user setup with feat testing

--- src/main/java/com/scaler/productservicejan31capstone/repositories/ProductRepository.java
+++ src/main/java/com/scaler/productservicejan31capstone/repositories/ProductRepository.java
@@ -1,8 +1,6 @@
 package com.scaler.productservicejan31capstone.repositories;
-
 import com.scaler.productservicejan31capstone.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 
 public interface  ProductRepository extends JpaRepository<Product, Long>
 {
--- src/main/java/com/scaler/productservicejan31capstone/services/ProductDBService.java
+++ src/main/java/com/scaler/productservicejan31capstone/services/ProductDBService.java
@@ -1,16 +1,29 @@
 package com.scaler.productservicejan31capstone.services;
 
 import com.scaler.productservicejan31capstone.exceptions.ProductNotFoundException;
+import com.scaler.productservicejan31capstone.models.Category;
 import com.scaler.productservicejan31capstone.models.Product;
+import com.scaler.productservicejan31capstone.repositories.CategoryRepository;
+import com.scaler.productservicejan31capstone.repositories.ProductRepository;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Primary
 public class ProductDBService implements ProductService
 {
+
+    ProductRepository productRepository;
+    CategoryRepository categoryRepository;
+
+    public ProductDBService(ProductRepository productRepository, CategoryRepository categoryRepository)
+    {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
 
     @Override
     public Product getProductById(long id) throws ProductNotFoundException {
@@ -23,7 +36,31 @@ public class ProductDBService implements ProductService
     }
 
     @Override
-    public Product createProduct(String name, String description, double price, String imageUrl, String category) {
-        return null;
+    public Product createProduct(String name, String description, double price, String imageUrl, String category)
+    {
+        Product product = new Product();
+        product.setName(name);
+        product.setDescription(description);
+        product.setPrice(price);
+        product.setImageUrl(imageUrl);
+
+        Category categoryObj = getCategoryFromDB(category);
+        categoryObj.setName(category);
+
+        product.setCategory(categoryObj);
+        return productRepository.save(product);
+    }
+
+    private Category getCategoryFromDB(String name)
+    {
+        Optional<Category> optionalCategory = categoryRepository.findByName(name);
+        if(optionalCategory.isPresent())
+        {
+            return optionalCategory.get();
+        }
+        Category category = new Category();
+        category.setName(name);
+
+        return categoryRepository.save(category);
     }
 }
